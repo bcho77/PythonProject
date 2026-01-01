b15446694a18f9ab79e3850882e7bfcf47be5e52
bcho77 <temgvan@gmail.com>
2026-01-01 23:23:44 +0100

    set for deployment

--- PortoFolio/portfolio/settings.py
+++ PortoFolio/portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=#30qry*#em9*tq&&fmap40abe@)11r_87=gs1@gb=n61+jz$y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
